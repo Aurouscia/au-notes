1bb42917b017fe6369f02782d4d2d7e0d9d414ca
Au <18971501210@189.cn>
2026-03-13 20:32:10 +0800

    feat(go): 函数进阶试题作答

--- Go/Exams/5.function/exam4-advanced-practice/main.go
+++ Go/Exams/5.function/exam4-advanced-practice/main.go
@@ -4,6 +4,29 @@ import "fmt"
 
 // 请在此实现所有函数
 
+func Logf(format string, args ...interface{}) {
+	fmt.Print("[LOG] ")
+	fmt.Println(fmt.Sprintf(format, args...))
+}
+
+func MakeAccumulator(initial float64) func(float64) float64 {
+	sum := 0.0
+	return func(x float64) float64 {
+		sum += x
+		return sum
+	}
+}
+
+func MakeMemoizedFib() func(int) int {
+	var m = make(map[int]int)
+	return func(x int) int {
+		if m[x] != 0 {
+
+		}
+		return 0
+	}
+}
+
 func main() {
 	fmt.Println("=== 函数高级特性实践题 ===")
 	// 请在此编写测试代码
